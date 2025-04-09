577515a075f3e5dfac85cde5e850c31ba3ca43f3
ComixHe <ComixHe1895@outlook.com>
2025-04-09 16:38:25 +0800

    build: update deps and use build extension
    Signed-off-by: ComixHe <ComixHe1895@outlook.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include "portaldemo.h"
 #include <QApplication>
+#include <chrono>
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
   PortalDemo demo;
   demo.show();
+  printf("prepare to show: %ld\n",
+         std::chrono::duration_cast<std::chrono::microseconds>(
+             std::chrono::system_clock::now().time_since_epoch())
+             .count());
   return QApplication::exec();
 }
